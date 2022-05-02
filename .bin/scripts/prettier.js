b4e613eb45445f4c675486d31b24f7214b6faefa
Adophilus <uchenna19of@gmail.com>
2022-05-02 16:35:08 +0100

    stopped prettier from running on deleted files

--- .bin/scripts/prettier.js
+++ .bin/scripts/prettier.js
@@ -4,10 +4,12 @@ const { exec } = require('child_process')
 const extensions = ['.html', '.css', '.js', '.jsx', '.vue']
 
 module.exports = (event, filePath) => {
-  for (let extension of extensions) {
-    if (filePath.endsWith(extension) && event === 'update') {
-      console.log(`prettifying ${filePath}`)
-      exec(`prettier --write "${path.join(process.cwd(), filePath)}"`)
+  if (event === "update") {
+    for (let extension of extensions) {
+      if (filePath.endsWith(extension) && event === 'update') {
+        console.log(`prettifying ${filePath}`)
+        exec(`prettier --write "${path.join(process.cwd(), filePath)}"`)
+      }
     }
   }
 }
